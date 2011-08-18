b50eb50c067a5af538c61674f7562b0712839783
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-18 02:14:22 -0700

    added email/create view and put password in input

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -8,6 +8,8 @@ class EmailController < ApplicationController
   end
 
   def input
+    params[:user][:password]              = "password"
+    params[:user][:password_confirmation] = "password"
     message = Mail.new params[:message]
 logger.fatal "message:\t#{message}"
 
