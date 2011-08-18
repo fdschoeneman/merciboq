dd1925bfc8433ab7d15727e69efe811d4e2a551b
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-17 21:41:32 -0700

    x

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,20 +4,20 @@ class EmailController < ApplicationController
 
   def input
     message = Mail.new params[:message]
-logger.debug "message:\t#{message}"
+logger.fatal "message:\t#{message}"
 
     from = message.from[0]
-logger.debug "from:\t#{from}"
+logger.fatal "from:\t#{from}"
     from_user = User.find_or_create_by_email(from) # TODO: grab name from email
-logger.debug "from_user:\t#{from_user}"
+logger.fatal "from_user:\t#{from_user}"
 
     message.to.each{|address|
-logger.debug "address:\t#{address}"
+logger.fatal "address:\t#{address}"
       to_user = User.find_or_create_by_email(address) # TODO: grab name
-logger.debug "to_user:\t#{to_user}"
+logger.fatal "to_user:\t#{to_user}"
       ty = Thankyou.create(:thanker => from_user, :welcomer => to_user, :headline => message.subject, :content => message.body)
-logger.debug "from:\t#{from}"
-logger.debug "ty:\t#{ty}"
+logger.fatal "from:\t#{from}"
+logger.fatal "ty:\t#{ty}"
     }
 
  #     message.attachments.each {|att|
