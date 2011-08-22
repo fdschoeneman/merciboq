eafc326d5e79ab306ac4f31c49765cafd6950e77
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-22 01:52:36 -0700

    welcomer_id

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -16,7 +16,7 @@ class ThankyouByEmailController < UsersController
                           :password => "password",
                           :password_confirmation => "password")
     message.to.each do
-      from_user.thankyous.create!(:welcomer => to_user.id,
+      from_user.thankyous.create!(:welcomer_id => to_user.id,
                                   :headline => message.subject,
                                   :content => message.body)
     end
