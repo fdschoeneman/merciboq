93007988f79d60d2cef822fb7a6b9e8d64f624e0
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-22 16:49:07 -0700

    blah2

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -15,16 +15,24 @@ class ThankyouByEmailController < UsersController
     to_user   = User.find_or_create_by_email(to,
                           :password => "password",
                           :password_confirmation => "password")
+    message.to.each{|address|
+logger.fatal "address:\t#{address}"
+      to_user = User.find_or_create_by_email(address) # TODO: grab name
+logger.fatal "to_user:\t#{to_user}"
+      ty = Thankyou.create!(:thanker => from_user.id, :welcomer => to_user.id, :headline => message.subject, :content => message.body)
+logger.fatal "from:\t#{from}"
+logger.fatal "ty:\t#{ty}"
+    }
 #    message.Thankyou.create!(:thanker_id => from_user.id,
 #                                 :welcomer_id => to_user.id,
 #                                 :headline => message.subject,
 #                                 :content => message.body)}
 
-    message.to.each { |to_user|
-      to_user.thankyou.create!(:thanker => from_user.id,
-                          :welcomer => to_user.id,
-                          :headline => message.subject,
-                          :content => message.body)}
+#    message.to.each { |to_user|
+#      to_user.thankyou.create!(:thanker => from_user.id,
+#                          :welcomer => to_user.id,
+#                          :headline => message.subject,
+#                          :content => message.body)}
 
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
