fcb30b60f090f49971dd2f6fe87a9c0611995d7a
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-22 03:29:48 -0700

    4 wheels

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -21,7 +21,7 @@ class ThankyouByEmailController < UsersController
 #                                 :content => message.body)}
 
     user = from_user
-    user.thankyou.new(:thanker_id => from_user.id,
+    user.Thankyou.new(:thanker_id => from_user.id,
                           :welcomer_id => to_user.id,
                           :headline => message.subject,
                           :content => message.body)
