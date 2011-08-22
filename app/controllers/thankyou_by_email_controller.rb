83389824f67aa208b70733ebc539bc58d821f4dc
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-21 21:58:40 -0700

    try this

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -15,7 +15,7 @@ class ThankyouByEmailController < ApplicationController
 #    to_user   = User.find_or_create_by_email(to)
     message.to.each{|address|
     to_user = User.find_or_create_by_email(to) # TODO: grab name
-    from_user.thankyou.create!(:welcomer => to_user.id, :headline => message.subject, :content => message.body)
+    from_user.thankyous.create!(:welcomer => to_user.id, :headline => message.subject, :content => message.body)
     }
 
 
