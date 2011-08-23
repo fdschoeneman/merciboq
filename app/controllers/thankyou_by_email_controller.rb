460b0297612236d33450fd31fca4a0fc795f2e03
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-22 19:24:23 -0700

    blah

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -6,26 +6,38 @@ class ThankyouByEmailController < UsersController
     message = Mail.new(params[:message])
     Rails.logger.log Logger::INFO, message.subject
     Rails.logger.log Logger::INFO, message.body.decoded
+    Rails.logger.log Logger::INFO, message.from
+    Rails.logger.log Logger::INFO, message.to
 
-    from      = message.from[0]
-    to        = message.to[0]
-    from_user = User.find_or_create_by_email(from,
-                          :password =>             "password",
-                          :password_confirmation => "password")
-    to_user   = User.find_or_create_by_email(to,
-                          :password => "password",
-                          :password_confirmation => "password")
-    message.to.each{|address|
-logger.fatal "address:\t#{address}"
-      to_user = User.find_or_create_by_email(address) # TODO: grab name
-logger.fatal "to_user:\t#{to_user}"
-#      from_user = User.find_or_create_by_email(message.from[0])
-      ty = Thankyou.new(:thanker => from, :welcomer => to, :headline => message.subject, :content => message.body)
-logger.fatal "from:\t#{from}"
-logger.fatal "ty:\t#{ty}"
-    ty.save
+    message.to.each do
+      from      = message.from[0]
+      to        = message.to[0]
+      from_user = User.find_or_create_by_email(from)
+      to_user   = User.find_or_create_by_email(to)
+      content   = message.body
+      headline  = message.subject
+      thankyou  = Thankyou.new(:thanker => from_user,
+                               :welcomer => to_user,
+                               :content => content,
+                               :headline => headline)
+      thankyou.save
+    end
+#                          :password =>             "password",
+#                          :password_confirmation => "password")
+#     to_user   = User.find_or_create_by_email(to,
+#                          :password => "password",
+#                          :password_confirmation => "password")
+#    message.to.each{|address|
+#logger.fatal "address:\t#{address}"
+#      to_user = User.find_or_create_by_email(address) # TODO: grab name
+#logger.fatal "to_user:\t#{to_user}"
+##      from_user = User.find_or_create_by_email(message.from[0])
+#      ty = Thankyou.new(:thanker => from, :welcomer => to, :headline => message.subject, :content => message.body)
+#logger.fatal "from:\t#{from}"
+#logger.fatal "ty:\t#{ty}"
+#    ty.save
 
-    }
+#    }
 #    message.Thankyou.create!(:thanker_id => from_user.id,
 #                                 :welcomer_id => to_user.id,
 #                                 :headline => message.subject,
@@ -37,7 +49,7 @@ logger.fatal "ty:\t#{ty}"
 #                          :headline => message.subject,
 #                          :content => message.body)}
 
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
-  end
+#    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+#  end
 end
 
