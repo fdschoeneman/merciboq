d1aa88b859ba7ec5adcf892cdf6858665b31f2e0
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-23 01:20:21 -0700

    blah

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -16,7 +16,7 @@ class ThankyouByEmailController < ThankyousController
       logger.fatal "address:\t#{address}"
       to_user = User.find_or_create_by_email(address) # TODO: grab name
       logger.fatal "to_user:\t#{to_user}"
-      ty = Thankyou.create!(:thanker => from_user.id, :welcomer => to_user.id, :headline => message.subject, :content => message.body)
+      ty = User.thankyou.create!(:thanker => from_user.id, :welcomer => to_user.id, :headline => message.subject, :content => message.body)
     logger.fatal "from:\t#{from}"
     logger.fatal "ty:\t#{ty}"
         }
