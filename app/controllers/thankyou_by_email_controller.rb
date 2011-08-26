6e57ea06fdb11345648949ce06208fb064565dce
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-26 03:13:59 -0700

    blah

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -8,7 +8,7 @@ class ThankyouByEmailController < UsersController
     to        = message.to[0]
     @from_user = User.find_or_create_by_email(from)
     @to_user   = User.find_or_create_by_email(to)
-    @content   = message.body.decoded.charset=ISO-8859-1
+    @content   = message.body.decoded.text
     @headline  = message.subject
     thankyou = Thankyou.new(params[:thankyou])
     Thankyou.new(:thanker_id => @from_user.id, :welcomer_id => @to_user.id,
