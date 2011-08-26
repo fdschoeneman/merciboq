826881f61f9656c3db1d37c369cb2ea4c8bed878
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-26 02:08:29 -0700

    blah

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -8,11 +8,11 @@ class ThankyouByEmailController < UsersController
     to        = message.to[0]
     @from_user = User.find_or_create_by_email(from)
     @to_user   = User.find_or_create_by_email(to)
-    @content   = message.content.plain
+#    @content   = message.content.plain
     @headline  = message.subject
     thankyou = Thankyou.new(params[:thankyou])
     Thankyou.new(:thanker_id => @from_user.id, :welcomer_id => @to_user.id,
-                 :content => @content, :headline => @headline).save( :validate => false )
+                 :content => "emailbody", :headline => @headline).save( :validate => false )
 
 # @thankyou = Thankyou.new(params[:thankyou][:thanker])
 #    @thankyou = Thankyou.new(params[:thankyou])
