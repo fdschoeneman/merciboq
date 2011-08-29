a30e4b9f3b9c37fe9f83d24920b3159eb4549d0c
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-08-28 22:35:14 -0700

    blah

--- app/controllers/thankyou_by_email_controller.rb
+++ app/controllers/thankyou_by_email_controller.rb
@@ -1,11 +1,9 @@
-class ThankyouByEmailController < UsersController
+class ThankyouByEmailController < ApplicationController
   require 'mail'
   skip_before_filter :verify_authenticity_token
 
   def create
     message = Mail.new params[:message]
-
-    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
     from      = message.from[0]
     to        = message.to[0]
@@ -13,47 +11,13 @@ class ThankyouByEmailController < UsersController
     @to_user   = User.find_or_create_by_email(to)
     @content   = message.text_part.body.decoded
     @headline  = message.subject
-    thankyou = Thankyou.new(params[:thankyou])
+    puts message.subject
+#    thankyou = Thankyou.new(params[:thankyou])
     Thankyou.new(:thanker_id => @from_user.id, :welcomer_id => @to_user.id,
                  :content => @content, :headline => @headline).save( :validate => false )
 
-# @thankyou = Thankyou.new(params[:thankyou][:thanker])
-#    @thankyou = Thankyou.new(params[:thankyou])
-#    @thankyou { :thanker => from_user }#,
-                           #:welcomer => to_user)#,
-                           #:content => content,
-                           #:headline => headline
-                        #   )
 
     render :text => 'success', :status => 200 # 404 would reject the mail
   end
-#                          :password =>             "password",
-#                          :password_confirmation => "password")
-#     to_user   = User.find_or_create_by_email(to,
-#                          :password => "password",
-#                          :password_confirmation => "password")
-#    message.to.each{|address|
-#logger.fatal "address:\t#{address}"
-#      to_user = User.find_or_create_by_email(address) # TODO: grab name
-#logger.fatal "to_user:\t#{to_user}"
-##      from_user = User.find_or_create_by_email(message.from[0])
-#      ty = Thankyou.new(:thanker => from, :welcomer => to, :headline => message.subject, :content => message.body)
-#logger.fatal "from:\t#{from}"
-#logger.fatal "ty:\t#{ty}"
-#    ty.save
-
-#    }
-#    message.Thankyou.create!(:thanker_id => from_user.id,
-#                                 :welcomer_id => to_user.id,
-#                                 :headline => message.subject,
-#                                 :content => message.body)}
-
-#    message.to.each { |to_user|
-#      to_user.thankyou.create!(:thanker => from_user.id,
-#                          :welcomer => to_user.id,
-#                          :headline => message.subject,
-#                          :content => message.body)}
-
-#  end
 end
 
---
+++ features/thankyou_by_email.feature
@@ -0,0 +1,16 @@
+Feature: Thankyou by email
+  In order to thank another person
+  As a person with an email address
+  I want to send a thankyou and have it published
+
+  Scenario: Registered user thanks another registered
+    Given A a user with an email of "n email has been sent to the application as a POST request
+    Then A a
+
+
+  Scenario: Registered user thanks unregistered
+
+  Scenario: Unregistered user thanks registered user
+
+  Scenario: Unregistered user thanks Unregistered user
+
