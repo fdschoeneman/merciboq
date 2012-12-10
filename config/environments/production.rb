5238f2b740affe3837a7b2da9888d7542297f086
Fred Schoeneman <fred.schoeneman@gmail.com>
2012-12-10 13:54:39 -0800

    changed domain back to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN'] # :domain         => 'heroku.com'
+  # :domain         => ENV['SENDGRID_DOMAIN'] 
+  :domain         => 'heroku.com'
 }
 
 ActionMailer::Base.delivery_method = :smtp
